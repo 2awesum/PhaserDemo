35b3560cf655e7782e480ce659b9df5a02296432
Dylan <dylan@Carls-iMac.local>
2016-06-18 13:19:00 -0700

    SpriteAnimation&Background

--- state0.js
+++ state0.js
@@ -1,28 +1,51 @@
-var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, doge, speed = 4;
+var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, doge, speed = 6;
 demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
-        game.load.image('doge', 'assets/sprites/Unknown.png');
+        game.load.spritesheet('doge', 'assets/spritesheets/doge.png', 512, 512);
+        game.load.image('tree', 'assets/background/treeBG.png')
     },
     create: function() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#80ff80';
         console.log('State0');
         addChangeStateEventListeners();
+        game.world.setBounds(0, 0, 2813, 1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        
+        var treeBG = game.add.sprite(0, 0, 'tree');
         doge = game.add.sprite(centerX, centerY, 'doge');
         doge.anchor.setTo(0.5, 0.5);
+        doge.scale.setTo(-0.6, 0.6);
+        game.physics.enable(doge);
+        doge.body.collideWorldBounds = true;
+        doge.animations.add('walk', [0, 1, 2, 3, 4, 5]);
+        
+        game.camera.follow(doge);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0,600, 1000);
+        
+        
     
     },
     update: function(){
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            doge.scale.setTo(-0.6, 0.6)
             doge.x += speed;
+            doge.animations.play('walk', 14, true)
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            doge.scale.setTo(0.6, 0.6)
             doge.x -= speed;
+            doge.animations.play('walk', 14, true)
         }
-                else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+        else {
+            doge.animations.stop('walk');
+            doge.frame = 0;
+        }
+                if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
             doge.y -= speed;
+                    if(doge.y < 395){
+                        doge.y = 395;
+                    }
         }
                 else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
             doge.y += speed;
